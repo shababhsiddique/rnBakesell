0e3eef719d26fec212377831f02f811a9ab8926c
shababhsiddique <shabab.h.siddique@gmail.com>
2018-08-23 11:48:14 +0600

    Lynda 5.4.28 bakesell api start

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+import DealList from './DealList';
+
+import ajax from '../ajax';
+
+export default class App extends Component {
+
+  state = {
+    deals: [],
+  };
+
+  async componentDidMount(){
+    const initDeals = await ajax.fetchInitialDeals();
+    this.setState( ()=>{
+      return {
+        deals: initDeals
+      };
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.deals.length > 0 ?(
+            <DealList deals = {this.state.deals}/>
+          ):(
+            <Text style={styles.header}>Bakesell</Text>
+          )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F8F7FA',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    fontSize:30,
+    color: '#454754',
+    textAlign: 'center'
+  }
+});
---
+++ src/components/DealList.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+export default class DealList extends Component {
+  render() {
+    return (
+      <View style={styles.dealContainer}>
+        <Text>Deals...</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  dealContainer: {
+
+  },
+});
